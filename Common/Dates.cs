a89c2bd54253bac9df308fbf847e1c6c6b54aa66
Kurokamori <abagaildavid.me@gmail.com>
2026-07-23 14:25:58 -0500

    init

---
+++ Common/Dates.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Bloom.Common
+{
+    public class Dates
+    {
+        public const string KeyFormat = "yyyy-MM-dd";
+
+        public static string Today() => DateTime.Now.ToString(KeyFormat, CultureInfo.InvariantCulture);
+
+        public static string Key(DateTime date) => date.ToString(KeyFormat, CultureInfo.InvariantCulture);
+
+        public static DateOnly Parse(string key) =>
+            DateOnly.ParseExact(key, KeyFormat, CultureInfo.InvariantCulture);
+
+        public static bool TryParse(string key, out DateOnly date) =>
+            DateOnly.TryParseExact(key, KeyFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out date);
+
+        public static string Pretty(DateOnly date) =>
+            date.ToString("dddd, MMMM d, yyyy", CultureInfo.InvariantCulture);
+
+        public static string Pretty(string key) =>
+            TryParse(key, out DateOnly date) ? Pretty(date) : key;
+
+        public static IEnumerable<DateOnly> Range(DateOnly start, DateOnly end)
+        {
+            for (DateOnly d = start; d <= end; d = d.AddDays(1))
+            {
+                yield return d;
+            }
+        }
+    }
+}
---
+++ Common/Json.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Bloom.Common
+{
+    public class Json
+    {
+        private static readonly JsonSerializerOptions Options = new()
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = false,
+            PropertyNameCaseInsensitive = true
+        };
+
+        public static string Write<T>(T value) => JsonSerializer.Serialize(value, Options);
+
+        public static T? Read<T>(string? text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            { 
+                return default; 
+            }
+            try
+            { 
+                return JsonSerializer.Deserialize<T>(text, Options); 
+            }
+            catch (JsonException)
+            { 
+                return default; 
+            }
+        }
+    }
+}
